b9f1275b2e4edd39ccedd215e6f922a538cbc000
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2023-04-01 13:46:57 -0300

    config fonts & theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,4 +6,18 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         box-sizing: border-box;
     }
+
+    :root {
+        font-size: 62.5%;
+    }
+
+    body, input, button {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.6rem;
+        line-height: 130%;
+    }
+
+    h1, h2, h3, h4, h5, h6 {
+        font-family: 'Baloo 2', cursive;
+    }
 `
